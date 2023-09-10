3f17111a141b1af64f9260c004601d2d21b1a266
Andriy Chernikov <chernikov@gmail.com>
2023-09-10 15:34:59 +0300

    I'll call you

--- Kaizen.Ng/src/app/app.component.ts
+++ Kaizen.Ng/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/models/user';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  
+  users : User[] = []; 
   title = 'kaizen';
+
+  constructor(private userService : UserService) {}
+
+  ngOnInit(): void {
+
+    this.userService.getAll().subscribe(data => {
+      this.users = data;
+    })
+    //this.users.push({id : 1, email : "email@email", "password" : "passwrd"});
+  }
+
+  onButtonClick() : void {
+    let user = new User();
+    user.id = 0;
+    user.email = "email@email.com";
+    user.password = "hifromfrontend";
+    this.userService.add(user).subscribe(data => {
+      console.log("User is saved");
+    });
+    
+  }
 }
