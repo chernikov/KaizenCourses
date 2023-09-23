46ca6d7a20b9376fb20f0342d4e2fc5d76d6b313
Andriy Chernikov <chernikov@gmail.com>
2023-09-23 10:53:39 +0300

    Add Register

---
+++ Kaizen.MainWeb/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Kaizen.Core;
+using Kaizen.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kaizen.MainWeb.Controllers;
+
+public class RegisterController  : Controller
+{
+    private readonly KaizenDbContext context;
+
+    public RegisterController(KaizenDbContext context) {
+        this.context = context;
+    }
+
+
+    [HttpPost]
+    public IActionResult Post([FromBody] User user)
+    {
+        context.Users.Add(user);
+        context.SaveChanges();
+        return Created("", user);
+    }
+}
