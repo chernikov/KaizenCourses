a323a60effa323418c31f3147acf6ea7dd9bbcc3
Andriy Chernikov <chernikov@gmail.com>
2023-09-24 09:07:18 +0300

    Change sass to scss

--- Kaizen.Ng/src/app/app.component.ts
+++ Kaizen.Ng/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { UserService } from 'src/services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   
