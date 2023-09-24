2b387ec8f161db06c477a942683d94566d3fdd94
Andriy Chernikov <chernikov@gmail.com>
2023-09-24 09:36:38 +0300

    Add TopMenu and pages

--- Kaizen.Ng/src/app/app.module.ts
+++ Kaizen.Ng/src/app/app.module.ts
@@ -6,15 +6,28 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import {MatButtonModule } from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { RouterModule } from '@angular/router';
+
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
